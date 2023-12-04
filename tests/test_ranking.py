f0eac1d74114dfff1e0a7d7f5f5676f867b2e470
Manu <mcabral@frba.utn.edu.ar>
2023-12-04 02:59:22 -0300

    feat(tests.ranking): add more checks

--- tests/test_ranking.py
+++ tests/test_ranking.py
@@ -2,9 +2,10 @@
 Test the Ranking stuff.
 """
 
+import os
 import unittest
 from pyfifa import Ranking, ranking_ids
-from pyfifa.ranking import FifaRankingId
+from pyfifa.ranking import FifaRankingId, RankingItem, FifaRankingId
 
 
 class TestRanking(unittest.TestCase):
@@ -28,6 +29,46 @@ class TestRanking(unittest.TestCase):
 
     def test_men_ranking(self):
         """> Should return a Ranking object."""
-        last_id = ranking_ids()[0]
-        ranking = Ranking(ranking_id=last_id)
+        ranking = Ranking()
         self.assertIsInstance(ranking, Ranking, msg="Should be a Ranking.")
+
+    def test_ranking_items(self):
+        """> Should return a list of RankingItem's (men and women)."""
+        ranking = Ranking()
+        self.assertIsInstance(ranking.items(), list, msg="Should be a list.")
+        self.assertIsInstance(
+            ranking.items()[0], RankingItem, msg="Should be a RankingItem."
+        )
+
+    def test_ranking_csv_export(self):
+        """> Should export a CSV file."""
+        filename = "ranking.csv"
+        ranking = Ranking()
+        ranking.export(extension="csv", filename=filename)
+        self.assertTrue(os.path.isfile(filename), msg="Should be exported.")
+        with open(filename, "r") as csv_file:
+            self.assertTrue(csv_file.readable(), msg="Should be readable.")
+        os.remove(filename)
+
+    def test_ranking_json_export(self):
+        """> Should export a JSON file."""
+        filename = "ranking.json"
+        ranking = Ranking()
+        ranking.export(extension="json", filename=filename)
+        self.assertTrue(os.path.isfile(filename), msg="Should be exported.")
+        with open(filename, "r") as json_file:
+            self.assertTrue(json_file.readable(), msg="Should be readable.")
+        os.remove(filename)
+
+    def test_ranking_limit(self):
+        """> Should return a Ranking object with a limit."""
+        ranking = Ranking(limit=10)
+        self.assertIsInstance(ranking, Ranking, msg="Should be a Ranking.")
+        self.assertEqual(len(ranking.items()), 10, msg="Should be 10 items.")
+
+    def test_ranking_id(self):
+        """> Should check if the default ranking id is a FifaRankingId."""
+        ranking = Ranking()
+        self.assertIsInstance(
+            ranking.id_, FifaRankingId, msg="Should be a FifaRankingId."
+        )
